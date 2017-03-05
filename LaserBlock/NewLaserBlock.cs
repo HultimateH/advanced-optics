e19f2b10f56d2d35f0567a41ac086858a5ce9edf
wangw571 <wang_w571@hotmail.com>
2017-03-04 22:19:37 -0500

    -Added Tube Renderer -Added Hold To fire thingy -Small other fixes

--- LaserBlock/NewLaserBlock.cs
+++ LaserBlock/NewLaserBlock.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using spaar.ModLoader;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using spaar.ModLoader;
 using UnityEngine;
 
 namespace ImprovedLaserBlock
@@ -32,11 +32,14 @@ namespace ImprovedLaserBlock
         protected MSlider BombHoldTimer;
         protected MMenu BombChargingPosition;
 
+        protected MToggle HoldingToEmit;
+
+        protected MToggle UseLegacy;
+
         protected MSlider LaserWidth;
 
         // Block-specific stuff
         private bool laserOnOff;
-
 
         private int CountDown;
         private int AlreadyCountDown;
@@ -52,7 +55,13 @@ namespace ImprovedLaserBlock
         public GameObject Especially;
         public Light PointLight;
 
+        public CustomTubeRenderer CTR;
+        public GameObject CTRGO;
+
         delegate void Init();
+        delegate void CheckIfNeeeeeeedsUpdate();
+
+        CheckIfNeeeeeeedsUpdate CINU;
 
         public override void SafeAwake()
         {
@@ -87,6 +96,10 @@ namespace ImprovedLaserBlock
 
             BombChargingPosition = AddMenu("ChargePosition", 0, new List<string> { "所有点添加粒子", "发射端添加粒子效果", "接触点添加粒子效果", "无粒子效果" });
 
+            HoldingToEmit = AddToggle("仅在按住时激发激光", "HoldOnly", false);
+
+            UseLegacy = AddToggle("使用传统渲染", "Legacy", false);
+
             // register mode switching functions with menu delegates
             LaserAbilityModeMenu.ValueChanged += CycleAbilityMode;
         }
@@ -120,6 +133,10 @@ namespace ImprovedLaserBlock
 
             BombChargingPosition = AddMenu("ChargePosition", 0, new List<string> { "All have particles", "Only Laser Block have particles", "Only Intersection have particles", "None" });
 
+            HoldingToEmit = AddToggle("Only emit laser when holding", "HoldOnly", false);
+
+            UseLegacy = AddToggle("Use Legacy Rending", "Legacy", false);
+
             // register mode switching functions with menu delegates
             LaserAbilityModeMenu.ValueChanged += CycleAbilityMode;
         }
@@ -128,13 +145,13 @@ namespace ImprovedLaserBlock
         protected override void BuildingUpdate()
         {
             PenetrativeLengthMultiplier.Value = Mathf.Clamp01(PenetrativeLengthMultiplier.Value);
-            LaserFocusSlider.DisplayInMapper = LaserEditModeMenu.Value == 1;
+            LaserFocusSlider.DisplayInMapper = LaserEditModeMenu.Value == 1 && UseLegacy.IsActive;
             LaserLengthSlider.DisplayInMapper = LaserEditModeMenu.Value == 1;
             PenetrativeLengthMultiplier.DisplayInMapper = LaserEditModeMenu.Value == 1;
             LaserWidth.DisplayInMapper = LaserEditModeMenu.Value == 1;
             LaserColourSlider.DisplayInMapper = LaserEditModeMenu.Value == 1;
             LaserOnOffToggle.DisplayInMapper = LaserEditModeMenu.Value == 1;
-
+            HoldingToEmit.DisplayInMapper = LaserEditModeMenu.Value == 1;
 
 
             LaserAbilityModeMenu.DisplayInMapper = LaserEditModeMenu.Value == 0;
@@ -183,25 +200,56 @@ namespace ImprovedLaserBlock
         protected override void OnSimulateStart()
         {
             rHInfos = new List<RHInfo>();
-            if (lr == null)
-            {
-                lr = this.gameObject.AddComponent<LineRenderer>();
-            }
-            lr.material = new Material(Shader.Find("Particles/Additive"));
-            lr.SetWidth(0.08f, 0.08f);
-
-            Color ArgColour = LaserColourSlider.Value;
-
-            lr.SetColors(Color.Lerp(ArgColour, Color.black, 0.45f),
-                Color.Lerp(ArgColour, Color.clear, 0.2f));
-            lr.SetVertexCount(0);
 
             Especially = new GameObject("TheThings");
+
+            if (!UseLegacy.IsActive)
+            {
+                CINU = new CheckIfNeeeeeeedsUpdate(CheckIfNeedsUpdate);
+                CTRGO = new GameObject("CubeTube");
+
+                if (CTR == null)
+                {
+                    CTR = CTRGO.AddComponent<CustomTubeRenderer>();
+                    //CTR.material = new Material(Shader.Find("Particles/Alpha Blended"));
+                    //CTR.crossSegments = 1;
+                    CTR.flatAtDistance = 1;
+                    CTR.maxRebuildTime = 0.01f;
+                }
+
+
+                CTRGO.transform.SetParent(transform);
+                CTRGO.transform.localPosition = Vector3.forward * 1.1f;
+                CTRGO.transform.rotation = transform.rotation;
+            }
+            else
+            {
+                CINU = new CheckIfNeeeeeeedsUpdate(LegacyCheckIfNeedsUpdate);
+                if (lr == null)
+                {
+                    lr = this.gameObject.AddComponent<LineRenderer>();
+                }
+                lr.material = new Material(Shader.Find("Particles/Additive"));
+                lr.SetWidth(0.08f, 0.08f);
+
+                Color ArgColour = LaserColourSlider.Value;
+
+                lr.SetColors(Color.Lerp(ArgColour, Color.black, 0.45f),
+                    Color.Lerp(ArgColour, Color.clear, 0.2f));
+                lr.SetVertexCount(0);
+            }
+
             Especially.transform.SetParent(this.transform);
             Especially.transform.localPosition = Vector3.forward * 1.1f;
             Especially.transform.LookAt(this.transform.position);
 
+            //lr.material.SetColors(Color.Lerp(ArgColour, Color.black, 0.45f),
+            //    Color.Lerp(ArgColour, Color.clear, 0.2f));
+
+
+
             laserOnOff = !LaserOnOffToggle.IsActive;
+
             PointLight = Especially.AddComponent<Light>();
             PointLight.color = LaserColourSlider.Value;
             PointLight.intensity = 3 * Math.Max(0.25f, PenetrativeLengthMultiplier.Value);
@@ -222,10 +270,6 @@ namespace ImprovedLaserBlock
                 PSCOLM.color = new Gradient() { alphaKeys = new GradientAlphaKey[] { new GradientAlphaKey(0, 0), new GradientAlphaKey(0.8f, 0.2f), new GradientAlphaKey(1, 1) }, colorKeys = new GradientColorKey[] { new GradientColorKey(PS.startColor, 0), new GradientColorKey(PS.startColor, 1) } };
                 PSCOLM.enabled = true;
 
-                ParticleSystem.InheritVelocityModule PSIV = PS.inheritVelocity;
-                PSIV.enabled = true;
-                PSIV.mode = ParticleSystemInheritVelocityMode.Initial;
-
                 PS.simulationSpace = ParticleSystemSimulationSpace.World;
 
                 //ParticleSystem.Burst BUTS = new ParticleSystem.Burst(10, 10, 1000);
@@ -240,46 +284,51 @@ namespace ImprovedLaserBlock
         {
             if (LaserOnOffKey.IsReleased)
             {
-                laserOnOff = !laserOnOff;
+                laserOnOff = HoldingToEmit.IsActive ?
+                    true : !laserOnOff;
             }
-            CheckIfNeedsUpdate(); // better to optimise more expensive stuff instead (eg. trig functions)
+            laserOnOff = HoldingToEmit.IsActive ? !LaserOnOffKey.IsDown : laserOnOff;
+
+            PointLight.enabled = !laserOnOff;
+            CINU(); // better to optimise more expensive stuff instead (eg. trig functions)
 
             if (!laserOnOff)
             {
                 doPassiveAbility();
             }
 
-            SetBeamWidth();
-
         }
         protected override void OnSimulateFixedUpdate()
         {
-            if (LaserAbilityModeMenu.Value == 3 && BombActivateKey.IsDown)
+            if (LaserAbilityModeMenu != null && LaserAbilityModeMenu.Value == 3)
             {
-                CountDown = (int)Mathf.Min(CountDown + 1, BombHoldTimer.Value * 100 + 1);
-                AlreadyCountDown = (int)Mathf.Min(AlreadyCountDown + 1, BombHoldTimer.Value * 100 + 1);
-                SetLights();
-                if (BombEffectOnOffToggle.IsActive && BombChargingPosition.Value != 2 && BombChargingPosition.Value != 3)
+                if (BombActivateKey.IsDown)
                 {
-                    Vector3 RandomPoint = EulerToDirection(UnityEngine.Random.Range(-360, 360), UnityEngine.Random.Range(-360, 360));
-                    Multiplier = AlreadyCountDown / (BombHoldTimer.Value * 100);
-                    SqrtMultiplier = Mathf.Sqrt(Multiplier);
-                    PS.Emit(
-                        Especially.transform.position + (RandomPoint * PS.startLifetime * 2 * Multiplier),
-                        -RandomPoint * 2 * Multiplier * Multiplier + this.Rigidbody.velocity,
-                        0.2f * Multiplier * Multiplier,
-                        PS.startLifetime * SqrtMultiplier,
-                        new Color(PS.startColor.r, PS.startColor.g, PS.startColor.b, 1));
+                    CountDown = (int)Mathf.Min(CountDown + 1, BombHoldTimer.Value * 100 + 1);
+                    AlreadyCountDown = (int)Mathf.Min(AlreadyCountDown + 1, BombHoldTimer.Value * 100 + 1);
+                    SetLights();
+                    if (BombEffectOnOffToggle.IsActive && BombChargingPosition.Value != 2 && BombChargingPosition.Value != 3)
+                    {
+                        Vector3 RandomPoint = EulerToDirection(UnityEngine.Random.Range(-360, 360), UnityEngine.Random.Range(-360, 360));
+                        Multiplier = AlreadyCountDown / (BombHoldTimer.Value * 100);
+                        SqrtMultiplier = Mathf.Sqrt(Multiplier);
+                        PS.Emit(
+                            Especially.transform.position + (RandomPoint * PS.startLifetime * 2 * Multiplier),
+                            -RandomPoint * 2 * Multiplier * Multiplier + this.Rigidbody.velocity,
+                            0.2f * Multiplier * Multiplier,
+                            PS.startLifetime * SqrtMultiplier,
+                            new Color(PS.startColor.r, PS.startColor.g, PS.startColor.b, 1));
+                    }
                 }
+                else
+                {
+                    CountDown = (int)(CountDown * 0.9f);
+                    AlreadyCountDown = (int)(AlreadyCountDown * 0.9f);
+                    SetLights();
+                }
+                PS.maxParticles = 100000;
+                PS.time -= Time.fixedDeltaTime * 2;
             }
-            else
-            {
-                CountDown = (int)(CountDown * 0.9f);
-                AlreadyCountDown = (int)(AlreadyCountDown * 0.9f);
-                SetLights();
-            }
-            PS.maxParticles = 100000;
-            PS.time -= Time.fixedDeltaTime * 2;
 
         }
         protected void Ignite(RHInfo rH)
@@ -353,7 +402,7 @@ namespace ImprovedLaserBlock
                         }
                         break;
                     case 3: // bomb
-                        if (BombEffectOnOffToggle.IsActive && BombChargingPosition.Value != 1 && BombChargingPosition.Value != 3)
+                        if (BombEffectOnOffToggle.IsActive && BombChargingPosition.Value != 1 && BombChargingPosition.Value != 3 && BombActivateKey.IsDown)
                         {
                             Vector3 POS = rH.point;
                             Vector3 RandomPoint = EulerToDirection(UnityEngine.Random.Range(-360, 360), UnityEngine.Random.Range(-360, 360));
@@ -414,39 +463,56 @@ namespace ImprovedLaserBlock
             }
         }
 
-        private LineRenderer lr;                // laser beam
-
         public Color colour;
+
+        public LineRenderer lr;
 
         // need to port over everything from the old version
         public List<RHInfo> rHInfos;
         public bool BeamHitAnything;            // also used by laser block for abilities
 
 
-        public void SetBeamWidth()
+        public void SetBeam()
         {
-            // temporary fix for people who REALLY want to change the laser width
-            lr.SetWidth(
-                Mathf.Max(this.transform.localScale.x, this.transform.localScale.y) * LaserWidth.Value * Mathf.Pow((BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ? 1 - (CountDown / (BombHoldTimer.Value * 100)) : 1), 2),
-                Mathf.Max(this.transform.localScale.x, this.transform.localScale.y) * LaserFocusSlider.Value * LaserWidth.Value * (LaserLength / LaserLengthSlider.Value) * Mathf.Pow((BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ? 1 - (CountDown / (BombHoldTimer.Value * 100)) : 1), 2));
+            Vector3 point = this.transform.position + this.transform.forward * 1.1f * this.transform.localScale.z;
+            CTR.SetPoints(
+                new Vector3[] {
+                point,
+                    //Mathf.Max(this.transform.localScale.x, this.transform.localScale.y) * LaserWidth.Value * Mathf.Pow((BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ? 1 - (CountDown / (BombHoldTimer.Value * 100)) : 1), 2),
+                point + this.transform.forward * LaserLength 
+                    //Mathf.Max(this.transform.localScale.x, this.transform.localScale.y) * 
+                    //LaserFocusSlider.Value * 
+                    //LaserWidth.Value * 
+                    //(LaserLength / LaserLengthSlider.Value) * 
+                    //Mathf.Pow(
+                    //    (BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ?
+                    //        1 - (CountDown / (BombHoldTimer.Value * 100)) : 1)
+                    //        , 2)
+                }
+                , LaserWidth.Value / 4 * Mathf.Pow((BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ? 1 - (CountDown / (BombHoldTimer.Value * 100)) : 1), 2)
+                , LaserColourSlider.Value);
+
+            CTR.crossSegments = (int)(LaserWidth.Value * 20 * 6);
         }
         public void SetLights()
         {
+            if (!PointLight.enabled) return;
             PointLight.intensity = 3 * Math.Max(0.25f, PenetrativeLengthMultiplier.Value) + 5 * Mathf.Pow((BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ? (CountDown / (BombHoldTimer.Value * 100)) : 0), 2);
             PointLight.range = 3 * Mathf.Max(this.transform.localScale.x, this.transform.localScale.y) * LaserWidth.Value + 5 * Mathf.Pow((BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ? (CountDown / (BombHoldTimer.Value * 100)) : 0), 2);
         }
         public void CheckIfNeedsUpdate()
         {
-
+            
             if (!laserOnOff)
             {
                 UpdateFromPoint(this.transform.TransformPoint(0, 0, 1.3f), this.transform.forward);
-                DrawBeam();
-                SetBeamWidth();
+                CTR.enabled = true;
+                SetBeam();
+                //DrawBeam();
             }
             else
             {
-                lr.SetVertexCount(0);
+                CTR.enabled = false;
             }
             //updateCount++;
             //if (updateCount > 5 || skipTickLimit)
@@ -455,6 +521,22 @@ namespace ImprovedLaserBlock
             //    updateCount = 0;
             //    return;
             //}
+
+        }
+
+        public void LegacyCheckIfNeedsUpdate()
+        {
+
+            if (!laserOnOff)
+            {
+                UpdateFromPoint(this.transform.TransformPoint(0, 0, 1.3f), this.transform.forward);
+                LegacyDrawBeam();
+                LegacySetBeamWidth();
+            }
+            else
+            {
+                lr.SetVertexCount(0);
+            }
 
         }
         private void UpdateFromPoint(Vector3 point, Vector3 dir)
@@ -489,26 +571,18 @@ namespace ImprovedLaserBlock
                 }
             }
         }
-        private void DrawBeam()
+
+        private void LegacyDrawBeam()
         {
-            //if (!laserOnOff)
-            //{
-            //    if (lLength != 0)
-            //    {
-            //        lLength = 0;
-            //        lr.SetVertexCount(0);
-            //    }
-            //    return;
-            //}
-            //lLength = beamPoints.Count;
-            //lr.SetVertexCount(lLength);
-            //for (int i = 0; i < lLength; i++)
-            //{
-            //    lr.SetPosition(i, beamPoints[i]);
-            //}
-            //BeamLastPoint = beamPoints[beamPoints.Count - 1];
             lr.SetVertexCount(2);
             lr.SetPositions(new Vector3[] { this.transform.position + this.transform.forward * 0.8f, this.transform.position + this.transform.forward * LaserLength });
+        }
+        private void LegacySetBeamWidth()
+        {
+            // temporary fix for people who REALLY want to change the laser width
+            lr.SetWidth(
+                Mathf.Max(this.transform.localScale.x, this.transform.localScale.y) * LaserWidth.Value * Mathf.Pow((BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ? 1 - (CountDown / (BombHoldTimer.Value * 100)) : 1), 2),
+                Mathf.Max(this.transform.localScale.x, this.transform.localScale.y) * LaserFocusSlider.Value * LaserWidth.Value * (LaserLength / LaserLengthSlider.Value) * Mathf.Pow((BombEffectOnOffToggle.IsActive && LaserAbilityModeMenu.Value == 3 ? 1 - (CountDown / (BombHoldTimer.Value * 100)) : 1), 2));
         }
 
 
@@ -517,6 +591,13 @@ namespace ImprovedLaserBlock
             float elevation = Elevation * Mathf.Deg2Rad;
             float heading = Heading * Mathf.Deg2Rad;
             return new Vector3(Mathf.Cos(elevation) * Mathf.Sin(heading), Mathf.Sin(elevation), Mathf.Cos(elevation) * Mathf.Cos(heading));
+        }
+
+
+
+        void LogHo()
+        {
+            Debug.Log("Ho");
         }
     }
 }
